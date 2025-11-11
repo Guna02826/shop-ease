2bed0384383735ec682edc6afd52f8f96df9486c
Gunasekaraselvapandian <gssp02826@gmail.com>
2025-11-11 22:39:42 +0530

    Refactor production route handling to ensure proper serving of index.html

--- server/src/server.js
+++ server/src/server.js
@@ -33,8 +33,12 @@ app.use("/api/orders", orderRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(path.resolve(), "client/build")));
 
-  app.get("/.*/", (req, res) => {
-    res.sendFile(path.join(path.resolve(), "client/build", "index.html"));
+  app.use((req, res, next) => {
+    if (!req.path.startsWith("/api")) {
+      res.sendFile(path.join(path.resolve(), "client/build", "index.html"));
+    } else {
+      next();
+    }
   });
 } else {
   app.get("/", (req, res) => {
