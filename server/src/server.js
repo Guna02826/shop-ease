a48108056f81b49234be2f1cccdb6c36a6c3268e
Gunasekaraselvapandian <gssp02826@gmail.com>
2025-11-11 22:29:19 +0530

    Update route handling for serving index.html in production

--- server/src/server.js
+++ server/src/server.js
@@ -33,7 +33,7 @@ app.use("/api/orders", orderRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(path.resolve(), "client/build")));
 
-  app.get("*", (req, res) => {
+  app.get("/:path*", (req, res) => {
     res.sendFile(path.join(path.resolve(), "client/build", "index.html"));
   });
 } else {
